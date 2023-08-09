9a474f16c6b91d751e71b0274db4b1b5efb19111
btbrad <24836323+btbrad@users.noreply.github.com>
2023-08-09 20:27:56 +0800

    feat: 处理body数据

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -9,11 +10,16 @@ function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig) {
   config.url = transformUrl(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig) {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig) {
+  return transformRequest(config.data)
 }
 
 export default axios
